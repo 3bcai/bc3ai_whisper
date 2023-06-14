6662e180bc2898766e2ce1f55257e85c75f3da51
seanbackstrom <sean.backstrom@gmail.com>
2023-06-13 17:58:01 -0700

    local model support

--- transcribe.py
+++ transcribe.py
@@ -21,7 +21,10 @@ def bc3ai_whisper(file_path, model="large-v2", device='cuda', translation_lang="
     assert os.path.isfile(file_path)
     assert device in ['cuda', 'cpu']
     assert output_format in ['json']
-    assert model in whisper.available_models()
+    if ".pt" in model:
+        pass
+    else:
+        assert model in whisper.available_models()
     assert translation_lang in [lang.code for lang in translator.get_target_languages()],f"TRANSLATION LANGUAGE NOT IN LANGUAGE CODES. MUST BE:\n{[lang.code for lang in translator.get_target_languages()]}"
     assert force_og_lang in [lang.code for lang in translator.get_source_languages()] or force_og_lang == 'auto', f"FORCED ORIGINAL LANGUAGE NOT IN LANGUAGE CODES. MUST BE:\n{[lang.code for lang in translator.get_source_languages()]}"
     assert mimetypes.guess_type(file_path)[0].split('/')[0] in ['audio', 'video']
